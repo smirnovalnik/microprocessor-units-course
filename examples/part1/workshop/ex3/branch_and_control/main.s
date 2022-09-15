eca8367e1c0b61d2976f9596b51590f4c123168e
Alexander Smirnov <alexander.smirnov@faradaydc.com>
2022-09-15 14:44:04 +0300

    добавлен пример лр 2

--- examples/part1/workshop/ex3/branch_and_control/main.s
+++ examples/part1/workshop/ex3/branch_and_control/main.s
@@ -22,12 +22,12 @@ Reset_Handler
             ;   counter = counter + 1
             ; Пусть переменная counter находится в R0
             CMP  R0, #42        ; Сравнение с числом 42
-            BLE  else_branch    ; Если меньше или равно, то переход else_branch
+            BLE  Else_Branch    ; Если меньше или равно, то переход Else_Branch
             MOVS R0, #0         ; counter = 0
-            B    if_done        ; Переход на if_done
-else_branch
+            B    If_Done        ; Переход на If_Done
+Else_Branch
             ADDS R0, R0, #1     ; counter = counter + 1
-if_done
+If_Done
 
 
             ; Цикл
@@ -40,68 +40,68 @@ if_done
             ; Пусть sum находится в R0, i в R1
             MOVS R0, #0         ; sum = 0
             MOVS R1, #0         ; i = 0
-loop1
+Loop1
             CMP  R1, #4         ; Сравнение i с числом 4
-            BGE  loop1_done     ; Если меньше, то переход loop1_done
+            BGE  Loop1_Done     ; Если меньше, то переход Loop1_Done
 
             ADDS R0, R0, R1     ; sum = sum + i
             ADDS R1, R1, #1     ; i = i + 1
-            B    loop1          ; повтор цикла
-loop1_done
+            B    Loop1          ; повтор цикла
+Loop1_Done
 
 
             ; Простой цикл
             ; Пусть количество повторов находится в R0
             MOVS R0, #5         ; Инициализация счетчика цикла
-loop2
+Loop2
             NOP                 ; Здесь нужно вставить полезные команды
             SUBS R0, R0, #1     ; Декремент счетчика цикла
-            BNE  loop2          ; Если не ноль, то переход loop2
+            BNE  Loop2          ; Если не ноль, то переход Loop2
 
             ; Опрос (polling) состояния регистра
             LDR  R0, =0x20000000 ; Загрузить адрес ячейки памяти или регистра
             MOVS R2, #0x10       ; Маска для бита 4 - 10000
-loop3
+Loop3
             LDR R1, [R0]        ; Прочитать состояние регистра
             TST R1, R2          ; Вычислить R1 И 0x4
-            BEQ loop3           ; Если ноль, то прочитать еще раз
+            BEQ Loop3           ; Если ноль, то прочитать еще раз
 
 
             ; Таблица переходов (многозначный выбор)
             ; Пусть номер случая находится в R0
             CMP  R0, #3         ; Сравнение с максимальным значением
-            BHI  default_case   ; Если больше, то переход default_case
+            BHI  Default_Case   ; Если больше, то переход Default_Case
             MOVS R2, #4         ; Умножение номер на 4
             MULS R0, R2, R0     ; (размер адреса)
-            LDR  R1,=BranchTable; Получение базового адреса таблицы переходов
+            LDR  R1,=Branch_Table; Получение базового адреса таблицы переходов
             LDR  R2, [R1, R0]   ; Загрузка адреса перехода со смещением
             BX   R2             ; Переход
             ALIGN 4             ; Выравнивание
             ; Таблица переходов с адресами для каждого случая
-BranchTable
+Branch_Table
             DCD  Case0
             DCD  Case1
             DCD  Case2
             DCD  Case3
-default_case
-            NOP                     ; Команды для случая по умолчанию
-            B    switch_done
+Default_Case
+            NOP             ; Команды для случая по умолчанию
+            B    Switch_Done
 Case0
-            NOP                     ; Команды для случая ‘0’
-            B    switch_done
+            NOP             ; Команды для случая ‘0’
+            B    Switch_Done
 Case1
-            NOP                     ; Команды для случая ‘1’
-            B    switch_done
+            NOP             ; Команды для случая ‘1’
+            B    Switch_Done
 Case2
-            NOP                     ; Команды для случая ‘2’
-            B    switch_done
+            NOP             ; Команды для случая ‘2’
+            B    Switch_Done
 Case3
-            NOP                     ; Команды для случая ‘3’
-            B    switch_done
-switch_done
+            NOP             ; Команды для случая ‘3’
+            B    Switch_Done
+Switch_Done
 
 
-Stop        B    Stop         ; Бесконечный цикл
+Stop        B    Stop       ; Бесконечный цикл
 
             ALIGN
 
