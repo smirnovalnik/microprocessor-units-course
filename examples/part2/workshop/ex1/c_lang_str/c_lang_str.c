71ab9023cc155e0b1b39645e7ea73916efea15a3
Alexander Smirnov <alexander.smirnov@faradaydc.com>
2023-02-05 15:10:06 +0300

    добавлен README

--- examples/part2/workshop/ex1/c_lang_str/c_lang_str.c
+++ examples/part2/workshop/ex1/c_lang_str/c_lang_str.c
@@ -8,10 +8,11 @@
   *          STM32F072RBT6 в среде разработки Keil uVision 5.
   *          Подключение библиотек поддержки МК STM32F072RBT6 осуществляется
   *          средствами IDE Keil через Run-Time Environment (RTE).
-  *          В программе показаны основы синтаксиса языка C.
+  *          В программе показано использование строк и потока вывода.
   ******************************************************************************
   */
 
+/* Подключение стандартной библиотеки ввода/вывода */
 #include "stdio.h"
 #include "EventRecorder.h"
 
@@ -51,28 +52,39 @@ void reverse(char *s)
 /* Функция main - точка входа в программу. */
 int main(void)
 {
+    /* Инициализация модуля EventRecorder для вывода в поток stdout. */
     EventRecorderInitialize(EventRecordAll, 1);
 
+    /* Печать строки в поток вывода (stdout).
+       Символ '\n' переводит курсор на новую строчку. */
     printf("Hello, world!\n");
 
+    /* Печать строки через %s */
     char str1[] = "Hello again";
     printf("Another string: %s\n", str1);
 
+    /* Печать содержимого массива.
+       %d - печать целочисленного числа со знаком */
     int a[] = {-2, -1, 0, 1, 2};
     for (int i = 0; i < 5; i++)
     {
         printf("%d\n", a[i]);
     }
 
+    /* Печать содержимого массива.
+       В строке используется два раза %d.
+       Первый раз печатается i, второй - a[i]. */
     for (int i = 0; i < 5; i++)
     {
         printf("a[%d]=%d\n", i, a[i]);
     }
 
+    /* Печать строки str2, преобразование строки str2 и
+       печать результата. */
     char str2[] = "reverse of the string";
     printf("str2: %s\n", str2);
     reverse(str2);
-    printf("str2: %s\n", str2);
+    printf("reversed str2: %s\n", str2);
 
     while(1);
 }
